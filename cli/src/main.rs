e4aaa250446ac4c8ba6c857986e2f2238a67ff57
madkarmaa <madkarma99@gmail.com>
2026-08-12 16:48:20 +0200

    feat(cli): add temporary basic code

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+use streamable::{Result, StreamableClient};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let client = StreamableClient::new()?;
+
+    let (client, _email, _password) = client.register(None, None, None).await?;
+    println!("signed in as {}", client.user().user_name);
+
+    let file = Path::new(env!("CARGO_MANIFEST_DIR")).join("../media/mp4-99mb-sample.mp4");
+    let video = client.upload_video(file, Some("My Video".into())).await?;
+    println!("https://streamable.com/{}", video.shortcode);
+
+    Ok(())
 }
